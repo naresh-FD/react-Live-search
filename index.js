17e15a86e3aaf0243ff1d88026ce0fb5859f0291
naresh-FD <mail2nareshfd@gmail.com>
2020-01-31 15:22:44 +0530

    1 file modified

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ class App extends Component {
 
   render() {
     return (
-      <div classname="container- p-0">
+      <div className="container- p-0">
         <SearchFilter />
       </div>
     );
